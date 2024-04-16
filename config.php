6331a7cd2705312a6576f420cc67d5104ada5929
YueJing76 <119746941+YueJing76@users.noreply.github.com>
2024-04-16 21:55:45 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,17 @@
-<?php
-$user 		= "root";
-$server 	= "localhost";
-$password 	= "";
-$db			= "styleme";
-$koneksi 	= mysqli_connect($server, $user, $password, $db);
-function is_logged_in() {}
+<?php session_start();
 
-if($koneksi == false)
-{
-	echo "Tidak Terkoneksi";
+$conn = mysqli_connect('localhost','','styleme');
+
+if(!$conn){
+    die('Database tidak terhubung');
 }
-?>
+
+$db = $conn->select_db('styleme');
+
+function is_logged_in(){
+    if(isset($_SESSION['username'])){
+        return true;
+    } else {
+        return false;
+    }
+}
