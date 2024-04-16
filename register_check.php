aa25686d7612cd7785984dee0200b5e03a148305
Adelia Tabita Sihotang <adeliatabitasihotang08@gmail.com>
2024-04-16 19:27:35 +0700

    ganti admin jd pengguna

--- register_check.php
+++ register_check.php
@@ -19,7 +19,7 @@
 	        //       exit();
 	    }
 	    else {
-	        $sql = "INSERT INTO admin(id, username, password, no_hp) VALUES ('$no_id','$username', '$password', '$no_handphone')";
+	        $sql = "INSERT INTO pengguna(id, username, password, no_hp) VALUES ('$no_id','$username', '$password', '$no_handphone')";
 	        if (mysqli_query($conn, $sql)){
 				// Pop-up versi CSS
 				header('Location: register.php#regissuccess');
