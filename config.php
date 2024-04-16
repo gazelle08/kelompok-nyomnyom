4f7df7bfdb452e9bffd66d3aae31ec13cf29c9c0
Adelia Tabita Sihotang <adeliatabitasihotang08@gmail.com>
2024-04-16 14:18:21 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,12 @@
-<?php session_start();
+<?php
+$user 		= "root";
+$server 	= "localhost";
+$password 	= "";
+$db			= "styleme";
+$koneksi 	= mysqli_connect($server, $user, $password, $db);
 
-$conn = mysqli_connect(); //ini belum 
-
-if(!$conn){
-    die('Database tidak terhubung');
+if($koneksi == false)
+{
+	echo "Tidak Terkoneksi";
 }
-
-$db = $conn->select_db('20si1');
-
-function is_logged_in(){
-    if(isset($_SESSION['username'])){
-        return true;
-    } else {
-        return false;
-    }
-}
+?>
