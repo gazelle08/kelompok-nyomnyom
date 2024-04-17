3eaccf9f589a8576ca2bf180b66040d38beef6a5
whiteplusyellow <elaine.agustina0501@gmail.com>
2024-04-17 10:07:32 +0700

    update

---
+++ input_form_laporan.php
@@ -0,0 +1,20 @@
+<?php
+include 'config.php';
+
+if(isset($_POST['tambah']))
+{
+
+$ambil_id_pesanan 	= $_POST['id_pesanan'];
+$ambil_status_laporan 			= $_POST['status_laporan'];
+$ambil_keterangan 			= $_POST['keterangan'];
+	
+mysqli_query ($koneksi, "INSERT INTO laporan-pelanggan
+			(id_pesanan,status_laporan, keterangan)
+			VALUES
+			('$ambil_id_pesanan', '$ambil_status_laporan', '$ambil_keterangan')");
+
+
+header ("location: laporan-pelanggan.php");
+
+}
+?>
