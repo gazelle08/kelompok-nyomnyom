b2efc08ede53c510f50a5f2819ad8d0a072709b6
YueJing76 <119746941+YueJing76@users.noreply.github.com>
2024-04-29 10:47:14 +0700

    ganti warna

--- produk_page.php
+++ produk_page.php
@@ -78,13 +78,13 @@ include 'config.php';
 		}
 		#button-ubah:hover{
 			background-color: white;
-			border: green;
-			color: green;
-			outline: 3px solid green;
+			border: #16FF00;
+			color: #16FF00;
+			outline: 3px #16FF00;
 			transition: 0.3s;
 		}
 		#button-ubah{
-			background-color: green;
+			background-color: #16FF00;
 			color: white;
 			font-family: montserrat semibold;
 			text-decoration: none;
